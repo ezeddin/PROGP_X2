44701e0a9c0da594b817a0b52f2c1f27b630b1d0
ezeddin <e.alhakim@gmail.com>
2015-04-24 16:02:04 +0200

    Final

--- erlang/bernoulli.erl
+++ erlang/bernoulli.erl
@@ -1,15 +1,13 @@
 -module(bernoulli).
--export([binom/2,berno/1,fac/1,be/1]).
- -include_lib("eunit/include/eunit.hrl").
+-export([berno/1,be/1]).
 be(N)-> [A|_]=(b(N)),
         {_,B}=A,
         B.
 b(0) ->[{0,1}];
 b(N) -> Bp=b(N-1), [{N, lists:foldr(fun({A, B}, Accin) ->Accin-(binom(N, A)/(N-A+1))*B end, 0, Bp)} | Bp].
 
-
-
-berno(0) -> 1;   
+% http://en.wikipedia.org/wiki/Bernoulli_number
+berno(0) -> 1;
 berno(N) -> - lists:sum(  [(binom(N, K)/(N - K + 1)) * berno(K)||K <- lists:seq(0,N - 1)]  ).
 
 binom(N,K)->
@@ -19,12 +17,3 @@ binom(_N,0,R) ->
 binom(N,K,R)->
     NewR = R * (N - K + 1)/K,
     binom(N,K - 1,NewR).
-
-fac(0) -> 1;
-fac(N) -> N * fac(N - 1).
-
-berno1_test() -> ?assert(berno(0) =:= 1).
-berno2_test() -> ?assert(berno(1) =:= - 1/2).
-berno3_test() -> ?assert(berno(2) =:= 1/6).
-    
-    
