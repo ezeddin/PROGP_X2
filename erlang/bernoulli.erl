df29e2b004c5a33e58366637675639cb557f36e9
felix engström <felixengstrom@n182-p146.kthopen.kth.se>
2015-04-24 15:25:49 +0200

    new ber

--- erlang/bernoulli.erl
+++ erlang/bernoulli.erl
@@ -1,34 +1,16 @@
 -module(bernoulli).
--export([binom/2,bern/1,berno/1,fac/1]).
+-export([binom/2,berno/1,fac/1,be/1]).
  -include_lib("eunit/include/eunit.hrl").
-bern(N) ->
-    B = array:new(N),
-    bern(array:set(0,1,B),1,N).
-    
-bern(B,M,N) when M == N ->
-    array:get(N-1,B);
+be(N)-> [A|_]=(b(N)),
+        {_,B}=A,
+        B.
+b(0) ->[{0,1}];
+b(N) -> Bp=b(N-1), [{N, lists:foldr(fun({A, B}, Accin) ->Accin-(binom(N, A)/(N-A+1))*B end, 0, Bp)} | Bp].
 
-bern(B0,M,N) ->
-    H = fun 
-        Help(B,K) when K == M ->
-            B;
-        Help(B,K) ->
-            X = array:get(M,B),
-            Y = binom(M + 1,K),
-            Z = array:get(K,B),
-            
-            io:format("X = ~p,~nY = ~p,~nZ = ~p~n~n",[X,Y,Z]),
-            Bm = X - (Y * Z),
-            NewB = array:set(M,Bm,B),
-            Help(NewB,K + 1)
-    end,
-    B1 = array:set(M,0,B0),
-    B2 = H(B1,0),
-    B3 = array:set(M,array:get(M,B2)/(M + 1),B2),
-    bern(B3,M+1,N).
+
 
 berno(0) -> 1;   
-berno(N) -> - lists:sum([1/(fac(K) * (N - K + 1)) * berno(K)||K <- lists:seq(0,N - 1)]).
+berno(N) -> - lists:sum(  [(binom(N, K)/(N - K + 1)) * berno(K)||K <- lists:seq(0,N - 1)]  ).
 
 binom(N,K)->
     binom(N,K,1).
