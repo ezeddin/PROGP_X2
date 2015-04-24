101a3e6a03bba2fbc6a977310ab56d24b2e57657
christoffer <cmoc@kth.se>
2015-04-24 11:44:36 +0200

    added unit tests

--- erlang/bernoulli.erl
+++ erlang/bernoulli.erl
@@ -1,6 +1,6 @@
 -module(bernoulli).
 -export([binom/2,bern/1,berno/1,fac/1]).
-
+ -include_lib("eunit/include/eunit.hrl").
 bern(N) ->
     B = array:new(N),
     bern(array:set(0,1,B),1,N).
@@ -29,12 +29,11 @@ bern(B0,M,N) ->
 
 berno(0) -> 1;   
 berno(N) -> - lists:sum([1/(fac(K) * (N - K + 1)) * berno(K)||K <- lists:seq(0,N - 1)]).
+
 binom(N,K)->
     binom(N,K,1).
-
 binom(_N,0,R) ->
     R;
-
 binom(N,K,R)->
     NewR = R * (N - K + 1)/K,
     binom(N,K - 1,NewR).
@@ -42,6 +41,8 @@ binom(N,K,R)->
 fac(0) -> 1;
 fac(N) -> N * fac(N - 1).
 
-
+berno1_test() -> ?assert(berno(0) =:= 1).
+berno2_test() -> ?assert(berno(1) =:= - 1/2).
+berno3_test() -> ?assert(berno(2) =:= 1/6).
     
     
